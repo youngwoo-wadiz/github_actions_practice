ec151904f441f9ef0bfd740345f3a6105a4c8d16
youngwoo.lee <youngwoo.lee@wadiz.kr>
2023-10-05 18:00:28 +0900

    Set SPM and cocoapods

---
+++ github_actions_practice/Pods/Target Support Files/Pods-github_actions_practice-github_actions_practiceUITests/Pods-github_actions_practice-github_actions_practiceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_github_actions_practice_github_actions_practiceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_github_actions_practice_github_actions_practiceUITests
+@end
